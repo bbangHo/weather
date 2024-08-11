ff9e8c9c76a9b1470a41d22d60a6f36936c10921
jul-ee <160218278+jul-ee@users.noreply.github.com>
2024-08-11 15:19:34 +0900

    [feat] 일정 거리 이상(translationX > 200) 오른쪽으로 스와이프 시 화면 전환

--- front/src/screens/HomeScreen.js
+++ front/src/screens/HomeScreen.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {ScrollView, View, Text, StyleSheet} from 'react-native';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const handleGesture = event => {
+    if (
+      event.nativeEvent.translationX > 200 &&
+      event.nativeEvent.state === State.END
+    ) {
+      navigation.navigate('PostCreationScreen');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <PanGestureHandler
+      onGestureEvent={handleGesture}
+      onHandlerStateChange={handleGesture}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.container}>
+          <Text>My</Text>
+        </View>
+      </ScrollView>
+    </PanGestureHandler>
   );
 };
 
