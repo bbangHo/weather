8b044b837a779c1ce4f42b275617b665a1d77fe9
seongsiyeong <ssy2681q@naver.com>
2024-08-17 15:22:28 +0900

    Openfeign과 gitignore 수정

--- back/src/main/java/org/pknu/weather/WeatherApplication.java
+++ back/src/main/java/org/pknu/weather/WeatherApplication.java
@@ -2,10 +2,12 @@ package org.pknu.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
 public class WeatherApplication {
 
 	public static void main(String[] args) {
