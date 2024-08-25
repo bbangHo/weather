d1134b2500c73a2225e233f95e654dba09fa09c7
seongsiyeong <ssy2681q@naver.com>
2024-08-25 23:41:33 +0900

    jwt 키 추가하기

--- back/src/main/java/org/pknu/weather/security/util/JWTUtil.java
+++ back/src/main/java/org/pknu/weather/security/util/JWTUtil.java
@@ -19,6 +19,7 @@ import java.util.Map;
 @Slf4j
 public class JWTUtil {
 
+    @Value("${spring.jwt.key}")
     private String key = "aweiogjw4oigj4oiwjgto2iw4joiw4joi234jw4j4802480";
 
     public String generateToken(Map<String, Object> valueMap, int days){
