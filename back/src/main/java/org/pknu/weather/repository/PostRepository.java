11b0b923ca98c4a8741b0d716b8a802e312b3866
bbangHo <sonsins1@naver.com>
2024-09-03 19:18:36 +0900

    [feat] 게시글 좋아요 기능 API
    - 커스텀 어노테이션, 관련 클래스 생성(파라미터로 넘어오는 Id 검증)

--- back/src/main/java/org/pknu/weather/repository/PostRepository.java
+++ back/src/main/java/org/pknu/weather/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package org.pknu.weather.repository;
 
+import org.pknu.weather.apiPayload.code.status.ErrorStatus;
 import org.pknu.weather.domain.Post;
+import org.pknu.weather.exception.GeneralException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
+    default Post safeFindById(Long postId) {
+        return findById(postId).orElseThrow(() -> new GeneralException(ErrorStatus._POST_NOT_FOUND));
+    }
 }
