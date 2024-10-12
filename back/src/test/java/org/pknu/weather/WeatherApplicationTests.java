4210ca0e530e00e039c2c42be3e826176fbde2a0
bbangHo <sonsins1@naver.com>
2024-10-12 16:22:01 +0900

    로컬에서 테스트 모두 통과 ci/cd 에서는 실패 ㅜ

--- back/src/test/java/org/pknu/weather/WeatherApplicationTests.java
+++ back/src/test/java/org/pknu/weather/WeatherApplicationTests.java
@@ -1,8 +1,11 @@
 package org.pknu.weather;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
 @EnableFeignClients
