771bc054f9990efc48a4e3cfb48cd587dff5195b
bbangHo <sonsins1@naver.com>
2024-11-06 21:18:16 +0900

    [fix] NPE 해결

--- back/src/main/java/org/pknu/weather/dto/converter/WeatherResponseConverter.java
+++ back/src/main/java/org/pknu/weather/dto/converter/WeatherResponseConverter.java
@@ -1,8 +1,5 @@
 package org.pknu.weather.dto.converter;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.List;
 import org.pknu.weather.common.utils.TagUtils;
 import org.pknu.weather.domain.Location;
 import org.pknu.weather.domain.Member;
@@ -12,6 +9,10 @@ import org.pknu.weather.domain.tag.TemperatureTag;
 import org.pknu.weather.dto.TagDto;
 import org.pknu.weather.dto.WeatherQueryResult;
 import org.pknu.weather.dto.WeatherResponse;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class WeatherResponseConverter {
 
@@ -75,7 +76,7 @@ public class WeatherResponseConverter {
 
     public static WeatherResponse.SimpleRainInformation toSimpleRainInformation(
             WeatherQueryResult.SimpleRainInfo simpleRainInfo) {
-        if (simpleRainInfo.getRain() == 0 && simpleRainInfo.getSnowCover() == 0) {
+        if (simpleRainInfo == null) {
             return WeatherResponse.SimpleRainInformation.builder()
                     .rainComment("오늘은 비소식이 없어요")
                     .addComment("")
