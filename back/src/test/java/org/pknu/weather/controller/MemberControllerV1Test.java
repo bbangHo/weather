f061381ed0b20ac7a8c655f1843c8c341e6ce7a2
bbangHo <sonsins1@naver.com>
2025-01-08 20:01:59 +0900

    test: 테스트 코드 수정

--- back/src/test/java/org/pknu/weather/controller/MemberControllerV1Test.java
+++ back/src/test/java/org/pknu/weather/controller/MemberControllerV1Test.java
@@ -1,173 +1,168 @@
-//package org.pknu.weather.controller;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import jakarta.persistence.EntityManager;
-//import org.junit.jupiter.api.Test;
-//import org.pknu.weather.common.TestDataCreator;
-//import org.pknu.weather.domain.Member;
-//import org.pknu.weather.domain.Terms;
-//import org.pknu.weather.domain.common.Sensitivity;
-//import org.pknu.weather.domain.common.TermsType;
-//import org.pknu.weather.dto.MemberJoinDTO;
-//import org.pknu.weather.dto.TermsDto;
-//import org.pknu.weather.repository.MemberRepository;
-//import org.pknu.weather.repository.MemberTermsRepository;
-//import org.pknu.weather.repository.TermsRepository;
-//import org.pknu.weather.security.util.JWTUtil;
-//import org.pknu.weather.service.MemberService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.mock.web.MockMultipartFile;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.Map;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//class MemberControllerV1Test {
-//
-//    @Autowired
-//    MockMvc mockMvc;
-//
-//    @Autowired
-//    MemberRepository memberRepository;
-//
-//    @Autowired
-//    TermsRepository termsRepository;
-//
-//    @Autowired
-//    MemberTermsRepository memberTermsRepository;
-//
-//    @Autowired
-//    MemberService memberService;
-//
-//    @Autowired
-//    EntityManager em;
-//
-//    @Autowired
-//    JWTUtil jwtUtil;
-//
-//    @Autowired
-//    ObjectMapper objectMapper;
-//
-//    @Test
-//    @Transactional
-//    void saveMemberInfo() throws Exception {
-//        // given
-//        init();
-//
-//        // MemberJoinDTO 객체 생성 (MockMultipartFile은 JSON에 포함되지 않음)
-//        MemberJoinDTO memberJoinDTO = MemberJoinDTO.builder()
-//                .nickname("nickname")
-//                .sensitivity(Sensitivity.NONE)
-//                .build();
-//
-//        byte[] fakeJpegData = new byte[]{
-//                (byte) 0xFF, (byte) 0xD8, // JPEG 시작 바이트
-//                // 간단한 더미 이미지 데이터
-//                (byte) 0xFF, (byte) 0xD9  // JPEG 종료 바이트
-//        };
-//
-//        // MockMultipartFile 생성 (profileImg로 사용)
-//        MockMultipartFile profileImgFile = new MockMultipartFile(
-//                "profileImg",  // 컨트롤러에서 바인딩될 필드 이름
-//                "test.jpg",
-//                "image/jpeg",
-//                fakeJpegData
-//        );
-//
-//        // TermsDto.Agreed 객체 생성
-//        TermsDto termsDtoAgreed = getTermsDto(true);
-//        String termsDtoJson = objectMapper.writeValueAsString(termsDtoAgreed);
-//
-//        // Member 객체 및 JWT 생성
-//        Member member = memberRepository.findMemberByEmail(TestDataCreator.getMember().getEmail()).get();
-//        String jwt = generateJwtToken(member.getId(), member.getEmail());
-//
-//        em.flush();
-//        em.clear();
-//
-//        // JSON 데이터를 MockMultipartFile로 변환
-//        MockMultipartFile memberJoinFile = new MockMultipartFile(
-//                "memberJoinDTO",
-//                "",
-//                "multipart/form-data",
-//                objectMapper.writeValueAsBytes(memberJoinDTO)  // profileImg 제외
-//        );
-//
-//        MockMultipartFile termsAgreed = new MockMultipartFile(
-//                "termsDto",
-//                "",
-//                "multipart/form-data",
-//                termsDtoJson.getBytes()
-//        );
-//
-//        // mockMvc를 사용하여 실제 컨트롤러 호출
-//        mockMvc.perform(multipart("/api/v1/member/info")
-//                        .file(profileImgFile)  // 파일 업로드
-//                        .file(memberJoinFile)  // MemberJoinDTO 데이터
-//                        .file(termsAgreed)    // TermsDto 데이터
-//                        .header("Authorization", jwt))  // Authorization 헤더
-//                .andExpect(status().isOk())  // 응답 상태 코드 200 OK 확인
-//                .andExpect(jsonPath("$.data.nickname").value(member.getNickname()))  // 응답 바디 검증
-//                .andExpect(jsonPath("$.data.email").value(member.getEmail()));  // 응답 바디 검증
-//    }
-//
-//    MemberJoinDTO getMemberJoinDTO() {
-//        return MemberJoinDTO.builder()
-//                .nickname("nickname")
-//                .profileImg(new MockMultipartFile("file", "test.jpg", "image/jpeg", "This is a test image.".getBytes()))
-//                .sensitivity(Sensitivity.NONE)
-//                .build();
-//    }
-//
-//    TermsDto getTermsDto(Boolean isRequired) {
-//        return TermsDto.builder()
-//                .isLocationServiceTermsAgreed(isRequired)
-//                .isServiceTermsAgreed(isRequired)
-//                .isPrivacyPolicyAgreed(isRequired)
-//                .isPushNotificationAgreed(isRequired)
-//                .build();
-//    }
-//
-//    @Transactional
-//    void init() {
-//        termsRepository.save(Terms.builder()
-//                .isRequired(true)
-//                .termsType(TermsType.SERVICE_TERMS)
-//                .termsVersion(1)
-//                .build());
-//
-//        termsRepository.save(Terms.builder()
-//                .isRequired(true)
-//                .termsType(TermsType.LOCATION_SERVICE_TERMS)
-//                .termsVersion(1)
-//                .build());
-//
-//        termsRepository.save(Terms.builder()
-//                .isRequired(true)
-//                .termsType(TermsType.PRIVACY_POLICY)
-//                .termsVersion(1)
-//                .build());
-//
-//        termsRepository.save(Terms.builder()
-//                .isRequired(false)
-//                .termsType(TermsType.PUSH_NOTIFICATION)
-//                .termsVersion(1)
-//                .build());
-//
-//        memberRepository.save(TestDataCreator.getMember());
-//    }
-//
-//    private String generateJwtToken(Long id, String email) {
-//        // JWT 토큰을 생성하는 메서드
-//        Map<String, Object> claims = Map.of("id", id,"email", email);
-//        return "Bearer " + jwtUtil.generateToken(claims ,1);
-//    }
-//}
+package org.pknu.weather.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.pknu.weather.common.TestDataCreator;
+import org.pknu.weather.domain.Member;
+import org.pknu.weather.domain.MemberTerms;
+import org.pknu.weather.domain.Terms;
+import org.pknu.weather.domain.common.TermsType;
+import org.pknu.weather.dto.MemberJoinDTO;
+import org.pknu.weather.dto.TermsDto;
+import org.pknu.weather.repository.MemberRepository;
+import org.pknu.weather.repository.MemberTermsRepository;
+import org.pknu.weather.repository.TermsRepository;
+import org.pknu.weather.security.util.JWTUtil;
+import org.pknu.weather.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MemberControllerV1Test {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    TermsRepository termsRepository;
+
+    @Autowired
+    MemberTermsRepository memberTermsRepository;
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    JWTUtil jwtUtil;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @Transactional
+    void saveMemberInfo() throws Exception {
+        // given
+        init();
+
+        em.flush();
+        em.clear();
+
+
+        MockMultipartFile profileImgFile = getProfileImage();
+        TermsDto termsDtoAgreed = getTermsDto(true);
+        Member member = memberRepository.findMemberByEmail(TestDataCreator.getMember().getEmail()).get();
+        MemberJoinDTO memberJoinDTO = getMemberJoinDTO(member);
+        String jwt = generateJwtToken(member.getId(), member.getEmail());
+
+
+        // mockMvc를 사용하여 실제 컨트롤러 호출
+        mockMvc.perform(multipart("/api/v1/member/info")
+                        .file(profileImgFile)  // 파일 업로드
+                        .param("sensitivity", memberJoinDTO.getSensitivity().toString())  // MemberJoinDTO 데이터
+                        .param("nickname", memberJoinDTO.getNickname())  // MemberJoinDTO 데이터
+                        .param("isServiceTermsAgreed", termsDtoAgreed.getIsServiceTermsAgreed().toString())    // TermsDto 데이터
+                        .param("isPrivacyPolicyAgreed", termsDtoAgreed.getIsPrivacyPolicyAgreed().toString())    // TermsDto 데이터
+                        .param("isLocationServiceTermsAgreed", termsDtoAgreed.getIsLocationServiceTermsAgreed().toString())    // TermsDto 데이터
+                        .param("isPushNotificationAgreed", termsDtoAgreed.getIsPushNotificationAgreed().toString())    // TermsDto 데이터
+                        .header("Authorization", jwt))  // Authorization 헤더
+                .andExpect(status().isOk())  // 응답 상태 코드 200 OK 확인
+                .andExpect(jsonPath("$.result.nickname").value(member.getNickname()))  // 응답 바디 검증
+                .andExpect(jsonPath("$.result.email").value(member.getEmail()));  // 응답 바디 검증
+
+        List<MemberTerms> terms = memberTermsRepository.findAll();
+        Assertions.assertThat(terms.size()).isEqualTo(4);
+    }
+
+    MockMultipartFile getProfileImage() throws IOException {
+        BufferedImage dummyImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(dummyImage, "jpeg", outputStream);
+        InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
+        // MockMultipartFile 생성
+        return new MockMultipartFile(
+                "profileImg",
+                "test.jpg",
+                "image/jpeg",
+                inputStream
+        );
+    }
+
+    MemberJoinDTO getMemberJoinDTO(Member member) {
+        return MemberJoinDTO.builder()
+                .nickname(member.getNickname())
+                .profileImg(new MockMultipartFile("file", "test.jpg", "image/jpeg", "This is a test image.".getBytes()))
+                .sensitivity(member.getSensitivity())
+                .build();
+    }
+
+    TermsDto getTermsDto(Boolean isRequired) {
+        return TermsDto.builder()
+                .isLocationServiceTermsAgreed(isRequired)
+                .isServiceTermsAgreed(isRequired)
+                .isPrivacyPolicyAgreed(isRequired)
+                .isPushNotificationAgreed(isRequired)
+                .build();
+    }
+
+    @Transactional
+    void init() {
+        termsRepository.save(Terms.builder()
+                .isRequired(true)
+                .termsType(TermsType.SERVICE_TERMS)
+                .termsVersion(1)
+                .build());
+
+        termsRepository.save(Terms.builder()
+                .isRequired(true)
+                .termsType(TermsType.LOCATION_SERVICE_TERMS)
+                .termsVersion(1)
+                .build());
+
+        termsRepository.save(Terms.builder()
+                .isRequired(true)
+                .termsType(TermsType.PRIVACY_POLICY)
+                .termsVersion(1)
+                .build());
+
+        termsRepository.save(Terms.builder()
+                .isRequired(false)
+                .termsType(TermsType.PUSH_NOTIFICATION)
+                .termsVersion(1)
+                .build());
+
+        memberRepository.save(TestDataCreator.getMember());
+    }
+
+    private String generateJwtToken(Long id, String email) {
+        // JWT 토큰을 생성하는 메서드
+        Map<String, Object> claims = Map.of("id", id,"email", email);
+        return "Bearer " + jwtUtil.generateToken(claims ,1);
+    }
+}
