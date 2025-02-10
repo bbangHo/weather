59a3075a5317cbe410db3769793e1535677eade7
bbangHo <sonsins1@naver.com>
2025-02-10 21:39:21 +0900

    ci: release 브랜치에서도 ci/cd가 작동하도록 임시 수정
    ci: 워크플로 수정
    
    - 개발 서버 도커 실행명 변경
    - 운영 서버 헬스체크 경로 수정

--- back/src/main/java/org/pknu/weather/aop/IntegrationLoggingAspect.java
+++ back/src/main/java/org/pknu/weather/aop/IntegrationLoggingAspect.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.pknu.weather.aop.util.ExecutionTimerUtils;
 import org.pknu.weather.aop.util.LoggingUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +14,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Aspect
 @Component
+@ConditionalOnProperty(value = "integration-logging", havingValue = "true", matchIfMissing = true)
 public class IntegrationLoggingAspect {
 
     /**
-     * Controller, Service 에 모든 메서드 수행을 로깅하는 AOP입니다.
-     * 쓰레드 id, 클래스명, 메서드명, 파라미터, 메서드 수행 시간 등이 로그에 남습니다.
+     * Controller, Service 에 모든 메서드 수행을 로깅하는 AOP입니다. 쓰레드 id, 클래스명, 메서드명, 파라미터, 메서드 수행 시간 등이 로그에 남습니다.
+     *
      * @param pjp
      * @return
      * @throws Throwable
