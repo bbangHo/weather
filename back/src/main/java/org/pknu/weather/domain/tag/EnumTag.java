7c17ea375a3dbefedd48c2c8a5ad8c04d4bc821d
bbangHo <sonsins1@naver.com>
2025-03-20 18:39:33 +0900

    refactor: TagUtils에 있던 비즈니스 로직을 Tag로 이동

--- back/src/main/java/org/pknu/weather/domain/tag/EnumTag.java
+++ back/src/main/java/org/pknu/weather/domain/tag/EnumTag.java
@@ -1,8 +1,6 @@
 package org.pknu.weather.domain.tag;
 
-import java.util.Optional;
-import org.pknu.weather.domain.ExtraWeather;
-import org.pknu.weather.domain.Weather;
+import org.pknu.weather.dto.TotalWeatherDto;
 
 public interface EnumTag {
     EnumTag findByCode(int code);
@@ -21,30 +19,14 @@ public interface EnumTag {
         return "";
     }
 
-    ;
-
     default String getTagName() {
         String[] split = getClass().toString().split("\\.");
         return split[split.length - 1];
     }
 
-    //    EnumTag weatherValueToTag(Weather weather, Optional<ExtraWeather> extraWeatherOptional);
-    EnumTag weatherValueToTag(Weather weather);
+    EnumTag weatherValueToTag(TotalWeatherDto totalWeatherDto);
 
-    default Boolean tagSelectedCheck(EnumTag tag, Weather weather, Optional<ExtraWeather> extraWeatherOptional) {
-        EnumTag enumTag = tag.weatherValueToTag(weather);
-        return enumTag == tag;
-
-//        if (tag instanceof WindTag) {
-//            return tag == WindTag.windSpeedToWindTag(weather.getWindSpeed());
-//        } else if (tag instanceof HumidityTag) {
-//            return tag == HumidityTag.humidityToHumidityTag(weather.getHumidity());
-//        } else if (tag instanceof SkyTag) {
-//            return tag == SkyTag.CLEAR;
-//        } else if (tag instanceof DustTag) {
-//            return extraWeatherOptional.map(extraWeather -> tag == DustTag.pmValueToDustTag(extraWeather))
-//                    .orElseGet(() -> tag == DustTag.GOOD);
-//        }
-//        return false;
+    default Boolean tagSelectedCheck(EnumTag tag, TotalWeatherDto totalWeatherDto) {
+        return tag == tag.weatherValueToTag(totalWeatherDto);
     }
 }
