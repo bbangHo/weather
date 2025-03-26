693dc78daa7c5be3d951e75ebca47940223b0594
bbangHo <sonsins1@naver.com>
2025-03-27 03:44:21 +0900

    refactor: 특정 지역에서 날씨 데이터가 없을 때 NPE가 발생하는 부분 처리

--- back/src/main/java/org/pknu/weather/repository/WeatherCustomRepositoryImpl.java
+++ back/src/main/java/org/pknu/weather/repository/WeatherCustomRepositoryImpl.java
@@ -9,11 +9,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.pknu.weather.apiPayload.code.status.ErrorStatus;
 import org.pknu.weather.common.formatter.DateTimeFormatter;
 import org.pknu.weather.common.utils.QueryUtils;
 import org.pknu.weather.domain.Location;
 import org.pknu.weather.domain.Weather;
 import org.pknu.weather.dto.WeatherQueryResult;
+import org.pknu.weather.exception.GeneralException;
 
 @RequiredArgsConstructor
 public class WeatherCustomRepositoryImpl implements WeatherCustomRepository {
@@ -68,7 +70,10 @@ public class WeatherCustomRepositoryImpl implements WeatherCustomRepository {
                 )
                 .fetchFirst();
 
-        assert weatherBaseTime != null;
+        if (weatherBaseTime == null) {
+            new GeneralException(ErrorStatus._WEATHER_DATA_NOT_FOUND_IN_THE_LOCATION);
+        }
+
         return weatherBaseTime.isEqual(baseTime);
     }
 
