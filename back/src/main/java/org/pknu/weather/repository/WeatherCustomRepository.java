dba4a0dc201b7142eb60c8990e7096867e8c2543
seongsiyeong <ssy2681q@naver.com>
2025-04-29 16:13:53 +0900

    feat: 요약 알림에 필요한 날씨 데이터 조회 메서드 작성

--- back/src/main/java/org/pknu/weather/repository/WeatherCustomRepository.java
+++ back/src/main/java/org/pknu/weather/repository/WeatherCustomRepository.java
@@ -1,10 +1,13 @@
 package org.pknu.weather.repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.pknu.weather.domain.Location;
 import org.pknu.weather.domain.Weather;
 import org.pknu.weather.dto.WeatherQueryResult;
+import org.pknu.weather.test.alarm.dto.WeatherSummaryDTO;
 
 public interface WeatherCustomRepository {
     boolean weatherHasBeenUpdated(Location location);
@@ -16,4 +19,6 @@ public interface WeatherCustomRepository {
     Weather findByLocationClosePresentationTime(Location location);
 
     Map<LocalDateTime, Weather> findAllByLocationAfterNow(Location location);
+
+    List<WeatherSummaryDTO> findWeatherSummary(Set<Long> locationIds);
 }
