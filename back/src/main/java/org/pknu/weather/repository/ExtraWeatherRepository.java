c4052d2ac2dbf0f1864ab30128330b931390fda6
seongsiyeong <ssy2681q@naver.com>
2025-04-30 21:04:47 +0900

    feat: 기타 날씨 조회 쿼리 작성

--- back/src/main/java/org/pknu/weather/repository/ExtraWeatherRepository.java
+++ back/src/main/java/org/pknu/weather/repository/ExtraWeatherRepository.java
@@ -1,12 +1,9 @@
 package org.pknu.weather.repository;
 
+import java.util.Set;
 import org.pknu.weather.domain.ExtraWeather;
-import org.pknu.weather.domain.Location;
-import org.pknu.weather.domain.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,4 +12,7 @@ import java.util.Optional;
 public interface ExtraWeatherRepository extends JpaRepository<ExtraWeather, Long> {
 
     Optional<ExtraWeather> findByLocationId(Long locationId);
+    @Query("select ew from ExtraWeather ew where ew.location.id in :locationIds and ew.basetime > :threeHoursAgo")
+    List<ExtraWeather> findExtraWeatherByLocations(Set<Long> locationIds, LocalDateTime threeHoursAgo);
+
 }
