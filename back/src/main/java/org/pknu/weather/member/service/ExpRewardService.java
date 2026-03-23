99c4b238779bff69ab3603bcc5e40cc829e049d3
bbangHo <sonsins1@naver.com>
2026-03-23 17:58:19 +0900

    chore: 단순 코드 수정

--- back/src/main/java/org/pknu/weather/member/service/ExpRewardService.java
+++ back/src/main/java/org/pknu/weather/member/service/ExpRewardService.java
@@ -19,7 +19,7 @@ public class ExpRewardService {
     private final MemberRepository memberRepository;
     private final Map<ExpEvent, ExpRewardLimitPolicy> policyMap;
 
-    @Async
+    @Async("ExpCUDExecutor")
     @Transactional
     public void rewardExp(String email, ExpEvent expEvent) {
         Member member = memberRepository.safeFindByEmail(email);
